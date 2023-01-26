98ec2dba8c823e6da8187c24074f2a137aa94e22
lakshmimurali <vmanikandan@zohocorp.com>
2023-01-26 16:09:01 +0530

    Form Control Basics

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from 'style.css';
+
+export default function App() {
+  return (
+    <div>
+      Demonstration of form elements to learn 1. One Way Data Binding 2.Two Way
+      Data Binding
+    </div>
+  );
+}
